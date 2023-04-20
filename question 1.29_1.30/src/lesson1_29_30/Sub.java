f1fd046c806cbb0b8e7b7f47296b8e311dcc35cc
obatarina <rina@192.168.0.20>
2023-04-20 22:47:58 +0900

    課題 java1.29 オブジェクト指向の基本②

---
+++ question 1.29_1.30/src/lesson1_29_30/Sub.java
@@ -0,0 +1,33 @@
+package lesson1_29_30;
+
+public class Sub {
+
+	private String name;                              
+	private String capital;
+	private int area;
+
+	public String getName() {                                //nameのgetterメソッド作成
+		return name;
+	}
+
+	public void setName(String name) {                       //nameのsetterメソッド作成
+		this.name = name;
+	}
+
+	public String getCapital() {                             //Capitalのgetterメソッド作成
+		return capital;
+	}
+
+	public void setCapital(String capital) {                 //Capitalのsetterメソッド作成
+		this.capital = capital;
+	}
+
+	public int getArea() {                                   //Areaのgetterメソッド作成
+		return area;
+	}
+
+	public void setArea(int area) {                          //Areaのsetterメソッド作成
+		this.area = area;
+	}
+
+}
