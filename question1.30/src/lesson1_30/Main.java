14c3061be63dbf9d55860537c1a940a0397f0794
obatarina <rina@192.168.0.20>
2023-04-24 18:56:56 +0900

    java　課題1.30 自己紹介プログラムを作成①

--- question1.30/src/lesson1_30/Main.java
+++ question1.30/src/lesson1_30/Main.java
@@ -2,7 +2,7 @@ package lesson1_30;
 
 class Main {
 	public static void main(String[] args) {
-		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);                        // 問題5:60を追加
+		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);                        //　60を追加
 		person1.print();                                                            // インスタンスメソッドprintの呼び出し
 
 	}
